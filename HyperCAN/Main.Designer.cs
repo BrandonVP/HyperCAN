5c0371339dd0b998275a3f3e1b6352fc79c7a843
Brandon Van Pelt <bvanpelt@hypertech.com>
2021-06-29 14:09:59 -0500

    Add project files.

---
+++ HyperCAN/Main.Designer.cs
@@ -0,0 +1,224 @@
+﻿namespace HyperCAN
+{
+    partial class main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(main));
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.toolStripDropDownButton1 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.openLogToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.saveCaptureToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripDropDownButton2 = new System.Windows.Forms.ToolStripDropDownButton();
+            this.appearanceToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.cOMPortToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.startButton = new System.Windows.Forms.Button();
+            this.pauseButton = new System.Windows.Forms.Button();
+            this.clearButton = new System.Windows.Forms.Button();
+            this.richCANBox = new System.Windows.Forms.RichTextBox();
+            this.fontSizeToolStripMenuItem = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStripComboBoxCOM = new System.Windows.Forms.ToolStripComboBox();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripDropDownButton1,
+            this.toolStripDropDownButton2});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(1008, 25);
+            this.toolStrip1.TabIndex = 3;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // toolStripDropDownButton1
+            // 
+            this.toolStripDropDownButton1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton1.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.openLogToolStripMenuItem,
+            this.saveCaptureToolStripMenuItem});
+            this.toolStripDropDownButton1.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton1.Image")));
+            this.toolStripDropDownButton1.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton1.Name = "toolStripDropDownButton1";
+            this.toolStripDropDownButton1.Size = new System.Drawing.Size(38, 22);
+            this.toolStripDropDownButton1.Text = "File";
+            // 
+            // openLogToolStripMenuItem
+            // 
+            this.openLogToolStripMenuItem.Name = "openLogToolStripMenuItem";
+            this.openLogToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.openLogToolStripMenuItem.Text = "Open Capture";
+            // 
+            // saveCaptureToolStripMenuItem
+            // 
+            this.saveCaptureToolStripMenuItem.Name = "saveCaptureToolStripMenuItem";
+            this.saveCaptureToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.saveCaptureToolStripMenuItem.Text = "Save Capture";
+            this.saveCaptureToolStripMenuItem.Click += new System.EventHandler(this.saveCaptureToolStripMenuItem_Click);
+            // 
+            // toolStripDropDownButton2
+            // 
+            this.toolStripDropDownButton2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripDropDownButton2.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.appearanceToolStripMenuItem,
+            this.cOMPortToolStripMenuItem});
+            this.toolStripDropDownButton2.Image = ((System.Drawing.Image)(resources.GetObject("toolStripDropDownButton2.Image")));
+            this.toolStripDropDownButton2.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.toolStripDropDownButton2.Name = "toolStripDropDownButton2";
+            this.toolStripDropDownButton2.Size = new System.Drawing.Size(62, 22);
+            this.toolStripDropDownButton2.Text = "Settings";
+            // 
+            // appearanceToolStripMenuItem
+            // 
+            this.appearanceToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fontSizeToolStripMenuItem});
+            this.appearanceToolStripMenuItem.Name = "appearanceToolStripMenuItem";
+            this.appearanceToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.appearanceToolStripMenuItem.Text = "Appearance";
+            // 
+            // cOMPortToolStripMenuItem
+            // 
+            this.cOMPortToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripComboBoxCOM});
+            this.cOMPortToolStripMenuItem.Name = "cOMPortToolStripMenuItem";
+            this.cOMPortToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.cOMPortToolStripMenuItem.Text = "COM Port";
+            this.cOMPortToolStripMenuItem.Click += new System.EventHandler(this.cOMPortToolStripMenuItem_Click);
+            // 
+            // startButton
+            // 
+            this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.startButton.Location = new System.Drawing.Point(4, 100);
+            this.startButton.Name = "startButton";
+            this.startButton.Size = new System.Drawing.Size(120, 40);
+            this.startButton.TabIndex = 4;
+            this.startButton.Text = "Start Capture";
+            this.startButton.UseVisualStyleBackColor = true;
+            this.startButton.Click += new System.EventHandler(this.startButton_Click);
+            // 
+            // pauseButton
+            // 
+            this.pauseButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.pauseButton.Location = new System.Drawing.Point(4, 146);
+            this.pauseButton.Name = "pauseButton";
+            this.pauseButton.Size = new System.Drawing.Size(120, 40);
+            this.pauseButton.TabIndex = 5;
+            this.pauseButton.Text = "Pause Capture";
+            this.pauseButton.UseVisualStyleBackColor = true;
+            this.pauseButton.Click += new System.EventHandler(this.pauseButton_Click);
+            // 
+            // clearButton
+            // 
+            this.clearButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.clearButton.Location = new System.Drawing.Point(4, 192);
+            this.clearButton.Name = "clearButton";
+            this.clearButton.Size = new System.Drawing.Size(120, 40);
+            this.clearButton.TabIndex = 6;
+            this.clearButton.Text = "Clear Capture";
+            this.clearButton.UseVisualStyleBackColor = true;
+            this.clearButton.Click += new System.EventHandler(this.clearButton_Click);
+            // 
+            // richCANBox
+            // 
+            this.richCANBox.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richCANBox.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.richCANBox.Font = new System.Drawing.Font("Courier New", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.richCANBox.Location = new System.Drawing.Point(130, 25);
+            this.richCANBox.Name = "richCANBox";
+            this.richCANBox.Size = new System.Drawing.Size(878, 704);
+            this.richCANBox.TabIndex = 7;
+            this.richCANBox.Text = "";
+            this.richCANBox.TextChanged += new System.EventHandler(this.richCANBox_TextChanged);
+            // 
+            // fontSizeToolStripMenuItem
+            // 
+            this.fontSizeToolStripMenuItem.Items.AddRange(new object[] {
+            "10",
+            "11",
+            "12",
+            "13",
+            "14",
+            "15",
+            "16",
+            "17",
+            "18"});
+            this.fontSizeToolStripMenuItem.Name = "fontSizeToolStripMenuItem";
+            this.fontSizeToolStripMenuItem.Size = new System.Drawing.Size(180, 23);
+            this.fontSizeToolStripMenuItem.Text = "Font Size";
+            this.fontSizeToolStripMenuItem.Click += new System.EventHandler(this.fontSizeToolStripMenuItem_Click);
+            // 
+            // toolStripComboBoxCOM
+            // 
+            this.toolStripComboBoxCOM.Name = "toolStripComboBoxCOM";
+            this.toolStripComboBoxCOM.Size = new System.Drawing.Size(180, 23);
+            this.toolStripComboBoxCOM.Click += new System.EventHandler(this.toolStripComboBoxCOM_Click);
+            // 
+            // main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(1008, 729);
+            this.Controls.Add(this.richCANBox);
+            this.Controls.Add(this.clearButton);
+            this.Controls.Add(this.pauseButton);
+            this.Controls.Add(this.startButton);
+            this.Controls.Add(this.toolStrip1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "main";
+            this.Text = "HyperCAN 1.0";
+            this.Load += new System.EventHandler(this.main_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton1;
+        private System.Windows.Forms.ToolStripMenuItem openLogToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem saveCaptureToolStripMenuItem;
+        private System.Windows.Forms.ToolStripDropDownButton toolStripDropDownButton2;
+        private System.Windows.Forms.ToolStripMenuItem appearanceToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem cOMPortToolStripMenuItem;
+        private System.Windows.Forms.Button startButton;
+        private System.Windows.Forms.Button pauseButton;
+        private System.Windows.Forms.Button clearButton;
+        private System.Windows.Forms.RichTextBox richCANBox;
+        private System.Windows.Forms.ToolStripComboBox fontSizeToolStripMenuItem;
+        private System.Windows.Forms.ToolStripComboBox toolStripComboBoxCOM;
+    }
+}
+
---
+++ HyperCAN/Main.cs
@@ -0,0 +1,214 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.IO.Ports;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+
+
+namespace HyperCAN
+{
+    public partial class main : Form
+    {
+        int SerialBaudRate = 500000;
+        String PortCOM = "COM10";
+        internal SaveFileDialog SaveFileDialog1;
+        public event EventHandler SelectionChangeCommitted;
+
+        public main()
+        {
+            InitializeComponent();
+            startSerial();
+            this.SaveFileDialog1 = new SaveFileDialog();
+        }
+
+        private void startSerial()
+        {
+            try
+            {
+                serialPort1.BaudRate = SerialBaudRate;
+                serialPort1.PortName = PortCOM;
+                serialPort1.Open();
+                serialPort1.DataReceived += serialPort1_DataReceived;
+            }
+            catch(Exception f)
+            {
+                MessageBox.Show("Unable to open " + PortCOM, "Error");
+            }
+        }
+
+        private void serialPort1_DataReceived(object sender, System.IO.Ports.SerialDataReceivedEventArgs e)
+        {
+            try
+            {
+                string line = serialPort1.ReadLine();
+                richCANBox.Invoke(new MethodInvoker(delegate { richCANBox.AppendText(line + "\n"); }));
+            }
+            catch(Exception f)
+            {
+
+            }
+        }
+        
+        private void mainBody_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void CANBox_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.WriteLine("0");
+            }
+            catch(Exception f)
+            {
+
+            }
+        }
+
+        private void pauseButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.WriteLine("1");
+            }
+            catch (Exception f)
+            {
+
+            }
+        }
+
+        private void clearButton_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                serialPort1.WriteLine("2");
+                richCANBox.Clear();
+            }
+            catch (Exception f)
+            {
+
+            }
+        }
+
+        private void main_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Declare a new memory stream.
+        MemoryStream userInput = new MemoryStream();
+
+        //saveFileDialog1.Filter = "Text File|*.txt|PCAN|*.TRC";
+        //System.IO.File.WriteAllLines(@"C:\Users\bvanpelt\source\repos\HyperCAN\HyperCAN\a.txt", richCANBox.Lines);
+        private void saveCaptureToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richCANBox.SaveFile(userInput, RichTextBoxStreamType.PlainText);
+            userInput.WriteByte(13);
+
+            // Display the entire contents of the stream,
+            // by setting its position to 0, to RichTextBox2.
+            userInput.Position = 0;
+            richCANBox.LoadFile(userInput, RichTextBoxStreamType.PlainText);
+
+            // Set the properties on SaveFileDialog1 so the user is 
+            // prompted to create the file if it doesn't exist 
+            // or overwrite the file if it does exist.
+            //SaveFileDialog1.CreatePrompt = true;
+            SaveFileDialog1.OverwritePrompt = true;
+
+            // Set the file name to myText.txt, set the type filter
+            // to text files, and set the initial directory to the 
+            // MyDocuments folder.
+            SaveFileDialog1.FileName = "Capture";
+            // DefaultExt is only used when "All files" is selected from 
+            // the filter box and no extension is specified by the user.
+            SaveFileDialog1.DefaultExt = "txt";
+            SaveFileDialog1.Filter =
+                "Text files (*.txt)|*.txt|All files (*.*)|*.*";
+            SaveFileDialog1.InitialDirectory =
+                Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+
+            // Call ShowDialog and check for a return value of DialogResult.OK,
+            // which indicates that the file was saved. 
+            DialogResult result = SaveFileDialog1.ShowDialog();
+            Stream fileStream;
+
+            if (result == DialogResult.OK)
+            {
+                // Open the file, copy the contents of memoryStream to fileStream,
+                // and close fileStream. Set the memoryStream.Position value to 0 
+                // to copy the entire stream. 
+                fileStream = SaveFileDialog1.OpenFile();
+                userInput.Position = 0;
+                userInput.WriteTo(fileStream);
+                fileStream.Close();
+            }
+        }
+
+        private void richCANBox_TextChanged(object sender, EventArgs e)
+        {
+            richCANBox.ScrollToCaret();
+        }
+
+        private void fontSizeToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.fontSizeToolStripMenuItem.SelectedIndexChanged +=
+            new System.EventHandler(fontSizeToolStripMenuItem_SelectedIndexChanged);
+        }
+        
+        private void fontSizeToolStripMenuItem_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int fontsize = 12;
+            fontsize = int.Parse(fontSizeToolStripMenuItem.SelectedItem.ToString());
+            richCANBox.Font = new Font("Courier New", fontsize);
+        }
+
+        protected void readPortNames()
+        {
+            // Clear current list to avoid duplication
+            this.toolStripComboBoxCOM.Items.Clear();
+
+            // Get a list of serial port names.
+            string[] ports = SerialPort.GetPortNames();
+
+            // Display each port name to the console.
+            foreach (string port in ports)
+            {
+                this.toolStripComboBoxCOM.Items.Add(port);
+            }
+        }
+
+        private void cOMPortToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            readPortNames();
+        }
+
+        private void toolStripComboBoxCOM_Click(object sender, EventArgs e)
+        {
+            this.toolStripComboBoxCOM.SelectedIndexChanged +=
+            new System.EventHandler(toolStripComboBoxCOM_SelectedIndexChanged);
+        }
+
+        private void toolStripComboBoxCOM_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            PortCOM = toolStripComboBoxCOM.SelectedItem.ToString();
+            serialPort1.Close();
+            startSerial();
+        }
+    }
+}
